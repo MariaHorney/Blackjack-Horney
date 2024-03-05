29d5264cbd2c146a7b9400944ec9b9771f83bdf0
MariaHorney <mghorney@my.waketech.edu>
2024-03-05 17:45:25 -0500

    Update ButtonBehavior.cs

--- Blackjack-Horney/Assets/ButtonBehavior.cs
+++ Blackjack-Horney/Assets/ButtonBehavior.cs
@@ -1,3 +1,11 @@
+/*
+Assignment/Lab/Project: Blackjack
+Name: Maria Horney
+Section: SGD.213.2172
+Instructor: Prof Brian Sowers
+Date: 03/11/2024
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
